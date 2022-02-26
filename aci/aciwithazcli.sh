4d819181e5a0b1015e7a705f9656c4e5fd299dff
Suriya Kalivardhan <suriyak@microsoft.com>
2022-02-26 07:25:10 -0800

    ACI with Subnet along with UMI and CLI

--- aci/aciwithazcli.sh
+++ aci/aciwithazcli.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-location='westcentralus'
+location='eastus'
 subscription=''
 resourcegroup=''
-aciname=$resourcegroup"aci0"
-uminame=$resourcegroup"umi0"
 
 if [ -z $1 ] || [ -z $2 ]; then
     echo "No Subscription or resource group passed, exiting.."
@@ -26,6 +24,13 @@ if [ rgid == '' ]; then
     exit 2
 fi
 
+set -x
+aciname=$resourcegroup"aci0"
+uminame=$resourcegroup"umi0"
+nsgname=$resourcegroup"umi0"
+vnetname=$resourcegroup"vnet0"
+subnetname=$resourcegroup"acisubnet0"
+set +x
 az configure -d location=$location subscription=$subscription group=$resourcegroup
 
 az identity create -n $uminame
@@ -33,6 +38,12 @@ umiid=`az identity show -n $uminame --query id | sed "s/\"//g"`
 #oid=`az identity show -n $uminame --query principalId | sed "s/\"//g"`
 #az role assignment create --role contributor --assignee-object-id $oid --assignee-principal-type ServicePrincipal --scope $rgid
 
-az container create -n $aciname --restart-policy OnFailure --image mcr.microsoft.com/azure-cli:latest --assign-identity $umiid  --command-line "/bin/bash -c 'az login --identity -u $umiid --allow-no-subscriptions; sleep infinity'"
+az network nsg create -n $nsgname
+nsgid=`az network nsg show -n $nsgname --query id | sed "s/\"//g"`
+az network vnet create -n $vnetname --address-prefix 10.0.0.0/16 --subnet-name $subnetname --subnet-prefix 10.0.0.0/24 --nsg $nsgid
+subnetid=`az network vnet subnet show --vnet-name $vnetname -n $subnetname --query id | sed "s/\"//g"`
+
+az container create -n $aciname --restart-policy OnFailure --image mcr.microsoft.com/azure-cli:latest --assign-identity $umiid --subnet $subnetid --command-line "/bin/bash -c 'az login --identity -u $umiid --allow-no-subscriptions; sleep infinity'"
+az container logs -n $aciname
 az container exec -n  $aciname --exec-command "az account get-access-token"
 az container exec -n  $aciname --exec-command "curl https://docs.microsoft.com/en-us/azure/container-instances/"
