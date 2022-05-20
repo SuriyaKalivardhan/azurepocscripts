d64c131a6de7837e36a028725865574d0c9183c8
Suriya Kalivardhan <suriyak@microsoft.com>
2022-05-20 12:05:03 -0700

    Delegated managed app with lock issue

--- mir/publicdeployment.sh
+++ mir/publicdeployment.sh
@@ -63,16 +63,3 @@ aiid=$(az monitor app-insights component show -a $appinsightsname --query id -o 
 
 az ml workspace create -n $workspace --set storage_account=$strid key_vault=$kvid container_registry=$acrid application_insights=$aiid 
 wsid=$(az resource show --resource-type Microsoft.MachineLearningServices/workspaces -n $workspace --query id -o tsv)
-
-az ml model create --name $workspace-model1 -v 1 -p .
-
-docker pull suriyakalivardhan/simpleinferencer:v1
-docker tag suriyakalivardhan/simpleinferencer:v1 $registryname.azurecr.io/suriyakalivardhan/simpleinferencer:v1
-az acr login -n $registryname
-docker push $registryname.azurecr.io/suriyakalivardhan/simpleinferencer:v1
-
-az ml online-endpoint create -n $epname --auth-mode=key
-az ml online-deployment create -e $epname -n $depname -f publicdep.yml --all-traffic
-token=$(az ml online-endpoint get-credentials -n $epname --query primaryKey -o tsv)
-
-curl -X POST https://$epname.$location.inference.ml.azure.com/inference -H "Authorization: Bearer $token" -d '{"Id":"1234", "Type": "Infer", "Input":"InferencingRequest"}'
