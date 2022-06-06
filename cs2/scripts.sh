e15618c57d6bef9bfc2434845db6f618363c3e03
Suriya Kalivardhan <suriyak@microsoft.com>
2022-06-06 07:31:32 -0700

    container scaleset versioning with multi containers

--- cs2/scripts.sh
+++ cs2/scripts.sh
@@ -3,7 +3,6 @@ az account set -s ea4faa5b-5e44-4236-91f6-5483d5b17d14
 az group create -n suriyakcs2poc --tags owner=suriyak@microsoft.com skipAutoDeleteTill=2022-09-01
 az cloud update --endpoint-resource-manager https://eastus2euap.management.azure.com
 az cloud list
-az rest -m put -b @profile.json -u https://eastus2euap.management.azure.com/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourceGroups/suriyakcs2poc/providers/Microsoft.ContainerInstance/containerGroupProfiles/suriyakcs2grouprofile2?api-version=2022-04-01-preview
-
-az rest -m put -b @cs2.json -u https://eastus2euap.management.azure.com/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourceGroups/suriyakcs2poc/providers/Microsoft.ContainerInstance/containerScaleSets/suriyakcs2instance2?api-version=2022-04-01-preview
+az rest -m put -b @profile.json -u https://eastus2euap.management.azure.com/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourceGroups/suriyakcs2poc/providers/Microsoft.ContainerInstance/containerGroupProfiles/suriyakcs2grouprofile4?api-version=2022-04-01-preview
+az rest -m put -b @cs2.json -u https://eastus2euap.management.azure.com/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourceGroups/suriyakcs2poc/providers/Microsoft.ContainerInstance/containerScaleSets/suriyakcs2instance4?api-version=2022-04-01-preview
 
