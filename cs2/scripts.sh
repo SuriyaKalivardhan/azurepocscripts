4630a73080ad29befd821aa95ae20c70e00e0594
Suriya Kalivardhan <suriyak@microsoft.com>
2022-06-06 06:57:38 -0700

    Container scaleset

---
+++ cs2/scripts.sh
@@ -0,0 +1,9 @@
+az configure -d subscription=ea4faa5b-5e44-4236-91f6-5483d5b17d14 group=suriyakcs2poc location=eastus2euap
+az account set -s ea4faa5b-5e44-4236-91f6-5483d5b17d14
+az group create -n suriyakcs2poc --tags owner=suriyak@microsoft.com skipAutoDeleteTill=2022-09-01
+az cloud update --endpoint-resource-manager https://eastus2euap.management.azure.com
+az cloud list
+az rest -m put -b @profile.json -u https://eastus2euap.management.azure.com/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourceGroups/suriyakcs2poc/providers/Microsoft.ContainerInstance/containerGroupProfiles/suriyakcs2grouprofile2?api-version=2022-04-01-preview
+
+az rest -m put -b @cs2.json -u https://eastus2euap.management.azure.com/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourceGroups/suriyakcs2poc/providers/Microsoft.ContainerInstance/containerScaleSets/suriyakcs2instance2?api-version=2022-04-01-preview
+
